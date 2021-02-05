14065d75be7c56f2826078d199e967640059b90f
ariyangd <57576103+ariyangd@users.noreply.github.com>
2021-02-06 00:28:15 +0800

    3rd

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+//let pakhi;
+// function add(num, num2){
+//     console.log(num+num2);
+//     return;
+// }
+// function add(num1, num2){
+//     console.log(num2);
+
+// }
+
+// const premik ={name:'smart dude', phone:234563};
+
+// console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
